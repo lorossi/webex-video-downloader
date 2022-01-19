c5bb63c2b9731e73be9db73a4a78580117f62e1f
lorossi <contact@lorenzoros.si>
2022-01-19 15:21:57 +0100

    improved extension failing to catch requests

--- src/background/background.js
+++ src/background/background.js
@@ -7,13 +7,37 @@ let elaborated_tab_ids = new Array(0); // tabs in which the button was added
  * @param {str} url
  * @returns json content of the url response
  */
-const getData = async (url) => {
-  const content = await fetch(url);
-  try {
-    return content.json();
-  } catch (E) {
-    return "";
-  }
+const getData = (url) => {
+  return fetch(url)
+    .then((content) => {
+      return content.json();
+    })
+    .catch((err) => {
+      return null;
+    });
+};
+
+/**
+ * Finds the id of the tab that made the request
+ *
+ * @param {str} video_id id of the video that is going to be extracted
+ * @param {Array} old_tabs array of tabs ids that have already been elaborated (a button is already placed there)
+ * @returns
+ */
+const getTab = (video_id, old_tabs) => {
+  // extract list of available tab
+  return chrome.tabs.query({}).then((tabs) => {
+    // loop through tabs and find one that has not been already elaborated
+    // and contains the video url
+    // keep looking until found
+    while (true) {
+      for (let i = 0; i < tabs.length; i++) {
+        if (!old_tabs.includes(tabs[i].id) && tabs[i].url.includes(video_id)) {
+          return tabs[i].id;
+        }
+      }
+    }
+  });
 };
 
 /**
@@ -34,27 +58,10 @@ const passRequest = async (request) => {
     const data = await getData(request.url);
     // extract video id
     const video_id = request.url.split("/").slice(-2, -1)[0];
-    // extract list of available tab
-    const tabs = await chrome.tabs.query({});
-
-    // loop through tabs and find one that has not been already elaborated
-    // and contains the video url
-    // keep looking until found
-    let tab_id;
-    let not_found = true;
-    while (not_found) {
-      for (let i = 0; i < tabs.length; i++) {
-        if (
-          !elaborated_tab_ids.includes(tabs[i].id) &&
-          tabs[i].url.includes(video_id)
-        ) {
-          tab_id = tabs[i].id;
-          elaborated_tab_ids.push(tab_id);
-          not_found = false;
-          break;
-        }
-      }
-    }
+    // find tab id
+    const tab_id = await getTab(video_id, elaborated_tab_ids);
+    // add tab id to list of already elaborated tabs
+    elaborated_tab_ids.push(tab_id);
 
     // pass message to tab
     chrome.tabs.sendMessage(tab_id, {
@@ -87,7 +94,8 @@ const downloadFile = (url, filename) => {
  * @param {int} tab_id
  */
 const removeElaborated = (tab_id) => {
-  elaborated_tab_ids.splice(elaborated_tab_ids.indexOf(tab_id), 1);
+  if (elaborated_tab_ids.includes(tab_id))
+    elaborated_tab_ids.splice(elaborated_tab_ids.indexOf(tab_id), 1);
 };
 
 // request listener
@@ -106,6 +114,5 @@ chrome.tabs.onRemoved.addListener(removeElaborated);
 // listener for updated tabs
 chrome.tabs.onUpdated.addListener((tab_id, change_info) => {
   // check if the page has been reloaded and if it was previously elaborated
-  if (change_info.status == "loading" && elaborated_tab_ids.includes(tab_id))
-    removeElaborated(tab_id);
+  if (change_info.status == "loading") removeElaborated(tab_id);
 });
